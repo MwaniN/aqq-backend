9be01109eae643d54d8fde031f19689e8a2ef8cd
MwaniN <brooklynewyork@gmail.com>
2025-02-24 23:42:14 -0500

    Note

--- db/index.js
+++ db/index.js
@@ -4,6 +4,7 @@ const config = require('../config.js');
 const client = new pg.Client(config);
 
 let connectIt = async function () {
+  // add try / catch for error handling
   await client.connect();
   return `db is connected on ${config.port}`;
 }
