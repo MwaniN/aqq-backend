a060a8f9ec426ac82ba1ca51135df4b76415f66f
MwaniN <brooklynewyork@gmail.com>
2025-02-28 15:20:56 -0500

    Models updates

--- db/models.js
+++ db/models.js
@@ -20,3 +20,33 @@ module.exports.signup = async function (req, res, userObject) {
 
   return
 }
+
+module.exports.login = async function (req, res, userObject) {
+
+  let uid = userObject.uid;
+
+  try {
+    let email = await client.query(`SELECT email FROM users WHERE id = '${uid}';`)
+
+    res.status(200).send({ email: `${email}`});
+    console.log('User logged in successfully');
+  } catch (error) {
+    console.log(error, " error logging in user")
+    res.status(400).send(error, " error logging in user")
+  }
+
+}
+
+module.exports.profile = async function (req, res, userObject) {
+  // replace with info for profile page
+  // username, email, date joined
+}
+
+module.exports.bookmarks = async function (req, res, userObject) {
+  // replace with bookmarked quotes for that user
+  // pull them from the SQL DB and paginate over a certain amount
+}
+
+module.exports.new_bookmark = async function (req, res, userObject) {
+  // add a new bookmark to the db for that user and return success to the client
+}
